6b2dee8f4ddce6f0ef55dbd96a409b640779dfbf
Brayan_Giraldo <giraldo8900@gmail.com>
2024-08-13 10:02:39 -0500

    practica3

--- script.js
+++ script.js
@@ -3,10 +3,56 @@ alert("Hola usuario, bienvenido a mi pagina");
 // Variables de JavaScript
 //   Hay 3 tipos de variables (var, let, const), las variables no tienen tipo de datos, las 3 variables almacenan cualquier tipo de dato
 
-var nombreCompleto = prompt("Ingrese su nombre completo: ");
+/* var nombreCompleto = prompt("Ingrese su nombre completo: ");
 console.log(nombreCompleto);
 var numeroUno = parseInt(prompt("Ingrese un numero: "));
 var numeroDos = parseInt(prompt("Ingrese un numero: "));
-console.log(numeroUno + numeroDos);  /* Se conoce como concatenar */
+console.log(numeroUno + numeroDos);  /* Se conoce como concatenar */ 
+
+// var nombreCompleto = "Brayan Giraldo";
+// var nombreCompleto = 2020;
+
+// console.log(nombreCompleto);
+
+// const saldoCuenta = 2000000;
+// saldoCuenta = 20000;
+// console.log(saldoCuenta);
+
+// Las variables de tipo var permiten la sobreescritura de variable y de contenido
+/* Las variables de tipo led no permiten sobreescritura de variables pero si de contenido */
+/* Las variables de tipo const no permiten sobreescritura de variables ni de contenido */
 
 // Todo dato ingresado por teclado es por defecto, un string, se tiene que hacer un parseo de datos (conversion de datos)
+
+/* Las variables de tipo let y const son el tipo de variables mas utilizadas hoy en dia */
+
+/* Datos de entrada */
+
+/* Datos de proceso 
+Operaciones aritmeticas y logicas
+Suma, resta, multiplicacion y division, comparacion, asignacion, aumento, decremento, etc
+
+Suma = valorUno + valorDos
+Resta = valorUno - valorDos
+Multiplicacion = valorUno * valorDos
+Division = valorUno / valorDos
+
+Logicos (Genera un resultado booleano: true o false);
+< -> Menor que (excluyentes)
+> -> Mayor que (Excluyentes)
+<= -> Menor o igual que (Incluyente)
+>= -> Mayor o igual que (Incluyente)
+== -> Comparacion Simple
+=== -> Comparacion Estricta
+!= -> Negacion Simple
+!== -> Negacion Compuesta
+
+*/
+
+console.log (3>5)
+console.log (3>3)
+console.log (3>2)
+console.log (3 == 3)
+console.log (3 ===3)
+
+/* Datos de salida */
