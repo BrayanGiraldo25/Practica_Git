28eac87d5a10440d1840ce39279f4f0b4385a98d
Brayan_Giraldo <giraldo8900@gmail.com>
2024-08-13 08:47:59 -0500

    practica2

---
+++ script.js
@@ -0,0 +1,12 @@
+alert("Hola usuario, bienvenido a mi pagina");
+/* prompt("Ingrese su nombre: ");  */// Codigo para que el usuario pueda ingresar su nombre
+// Variables de JavaScript
+//   Hay 3 tipos de variables (var, let, const), las variables no tienen tipo de datos, las 3 variables almacenan cualquier tipo de dato
+
+var nombreCompleto = prompt("Ingrese su nombre completo: ");
+console.log(nombreCompleto);
+var numeroUno = parseInt(prompt("Ingrese un numero: "));
+var numeroDos = parseInt(prompt("Ingrese un numero: "));
+console.log(numeroUno + numeroDos);  /* Se conoce como concatenar */
+
+// Todo dato ingresado por teclado es por defecto, un string, se tiene que hacer un parseo de datos (conversion de datos)
